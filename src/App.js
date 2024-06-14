2c38da76195af1042f07c1b00b64b3727d03102e
Jeon Seoungho <pcjoker10@gmail.com>
2024-06-15 07:26:27 +0900

    [4/n] Add TodoItemList react component to display todo items: Add onSubmit callback to add newTodoItem to todoItems state

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
+let todoItemId = 0;
 
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
+
   const onSubmit = () => {
     props.onSubmit(input);
     setInput("");
@@ -22,12 +24,32 @@ const TodoItemInputField = (props) => {
   </div>);
 };
 
+const TodoItemList = (props) => {
+  const todoList = props.todoItemList.map((todoItem, index) => {
+    return <li key={index}>{todoItem.todoItemContent}</li>;
+  });
+  return (<div>
+    <ul>{todoList}</ul>
+  </div>);
+};
+
 function App() {
-return (
-  <div className="App">
-    <TodoItemInputField onSubmit={() => {}} />
-  </div>
-);
+  const [todoItemList, setTodoItemList] = useState([]);
+
+  const onSubmit = (newTodoItem) => {
+    setTodoItemList([...todoItemList, {
+      id: todoItemId,
+      todoItemContent: newTodoItem,
+      isFinished: false,
+    }]);
+  };
+
+  return (
+    <div className="App">
+      <TodoItemInputField onSubmit={onSubmit} />
+      <TodoItemList todoItemList={todoItemList} />
+    </div>
+  );
 }
 
 export default App;
